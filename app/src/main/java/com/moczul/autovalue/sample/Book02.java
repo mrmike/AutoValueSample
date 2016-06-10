217653e85a484c8ce6c5043cbd5e66a2b7ace886
Michal Moczulski <moczul@gmail.com>
2016-06-10 08:54:12 +0200

    Fix typo

--- app/src/main/java/com/moczul/autovalue/sample/Book02.java
+++ app/src/main/java/com/moczul/autovalue/sample/Book02.java
@@ -7,8 +7,8 @@ import com.google.auto.value.AutoValue;
 @AutoValue
 public abstract class Book02 {
 
-    public static Book02 create(String title, @Nullable String author, int pubYer, int pageCount) {
-        return new AutoValue_Book02(title, author, pubYer, pageCount);
+    public static Book02 create(String title, @Nullable String author, int pubYear, int pageCount) {
+        return new AutoValue_Book02(title, author, pubYear, pageCount);
     }
 
     abstract String title();
